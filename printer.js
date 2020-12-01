984d5ab2e59f10be805a8122cc2647951835e5bf
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-01 11:07:09 +0000

    printStatement prints correctly formatted statement

--- printer.js
+++ printer.js
@@ -1,4 +1,11 @@
 export default class Printer {
+  printStatement(statement) {
+    this.printHeader()
+    statement.map((transaction) => {
+      console.log(transaction.date + " || " + transaction.credit + " || " + transaction.debit + " || " + transaction.balance)
+    })
+  }
+
   printHeader() {
     console.log("date || credit || debit || balance")
   }
--- printer.test.js
+++ printer.test.js
@@ -6,10 +6,14 @@ describe('Printer', () => {
 
   beforeEach(() => {
     printer = new Printer();
-    mockStatement = [{date: '21/10/2015', type: "credit", amount: 100}, {date: '21/10/2015', type: "debit", amount: 50}]
+    mockStatement = [{date: '21/10/2015', credit: 100, debit: "", balance: 150}, {date: '21/10/2015', debit: 50, credit: "", balance: 20}]
     global.console = {
       log: jest.fn(),
     }
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks();
   })
 
   describe('printHeader', () => {
@@ -18,4 +22,13 @@ describe('Printer', () => {
       expect(global.console.log).toHaveBeenCalledWith("date || credit || debit || balance")
     })
   })
+
+  describe('printStatement', () => {
+     it('prints the statement and header', () => {
+       printer.printStatement(mockStatement)
+       expect(global.console.log).toHaveBeenCalledWith("date || credit || debit || balance")
+       expect(global.console.log).toHaveBeenCalledWith("21/10/2015 ||  || 50 || 20")
+       expect(global.console.log).toHaveBeenCalledWith("21/10/2015 || 100 ||  || 150")
+     })
+   })
 })
